f698af9af044032fc2ab272b087c75ae2c0788d4
Jens Rösing <roesing.jens@gmx.de>
2024-01-27 13:30:43 +0100

    working on character animation system

---
+++ Assets/Scripts/Enemy/ClownAnimator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownAnimator : MonoBehaviour
+{
+    private static ClownAnimator instance;
+    private const string Walking = "walking";
+    private const string Watching = "watching";
+    private const string Running = "running";
+    private const string WalkingSpeed = "walkSpeed";
+    private const string Death = "death";
+
+    [SerializeField] private Animator _animator;
+
+    private Dictionary<AnimationStates, string> _states = new Dictionary<AnimationStates, string>();
+
+    public enum AnimationStates
+    {
+        Idle = 0,
+        Walking = 1,
+        Watching = 2,
+        Running = 3,
+        Death = 4,
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        //_states.Add(AnimationStates.Idle, "");
+        _states.Add(AnimationStates.Walking, Walking);
+        _states.Add(AnimationStates.Watching, Watching);
+        _states.Add(AnimationStates.Running, Running);
+        //_states.Add(AnimationStates.Death, Death);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public void SetAnimationState(AnimationStates newAnimationState)
+    {
+        // Iteriere durch die AnimationStates im Dictionary
+        foreach (var state in _states)
+        {
+            // Aktiviere/deaktiviere den bool-Wert basierend auf dem aktuellen Zustand
+            bool isActive = state.Key == newAnimationState;
+            _animator.SetBool(state.Value, isActive);
+        }
+    }
+
+    public static ClownAnimator GetInstance()
+    {
+        return instance;
+    }
+}
