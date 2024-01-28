4ebb8cf2f4daf94cd0917df3ba25d8b67c11f9cf
Max Fock <mfo@sisyfox.com>
2024-01-28 14:08:29 +0100

    textfalsh script added

--- Assets/Scripts/TextFlash.cs
+++ Assets/Scripts/TextFlash.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextFlash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private float _speed = 1f;
 
+    private float timer;
+    private Color _color;
     // Update is called once per frame
+
+    private void Start()
+    {
+        _color = text.color;
+    }
     void Update()
     {
-        
+        if (text == null && _speed > 0)
+        {
+            timer += Time.deltaTime * _speed;
+            _color.a = Mathf.Sin(timer);
+            text.color = _color;
+        }
+
     }
 }
