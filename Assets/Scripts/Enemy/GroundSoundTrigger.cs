c3da37ce6888433fa9a3d32459a5dabf3ea0c847
Max Fock <mfo@sisyfox.com>
2024-01-28 14:34:29 +0100

    plank sounds

--- Assets/Scripts/Enemy/GroundSoundTrigger.cs
+++ Assets/Scripts/Enemy/GroundSoundTrigger.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class GroundSoundTrigger : MonoBehaviour
 {
+    [SerializeField] private AudioClip sound;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            GetComponent<AudioSource>().PlayOneShot(sound);
+
             // ToDo Player a Sound
             Debug.Log("Player Triggered a Sound Trap");
             ClownAgent.GetInstance().SetNewDestination(transform.position);
